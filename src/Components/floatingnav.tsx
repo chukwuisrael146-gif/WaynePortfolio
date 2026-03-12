df4c8b8fd63ccc4571cc076d193c0a60157eae78
chukwuisrael146-gif <chukwuisrael146@gmail.com>
2026-03-12 13:45:35 +0100

    some fixes

--- src/Components/floatingnav.tsx
+++ src/Components/floatingnav.tsx
@@ -13,6 +13,7 @@ import {
 export default function RightNav() {
     return (
         <div className="
+        hidden md:flex
   fixed left-4 top-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-md border border-white/20 shadow-lg 
   flex-col items-center jusfify-center gap-10 p-5 mr-2 
   bg-black/50 backdrop-blur-md rounded-[50px]
@@ -35,10 +36,10 @@ function IconButton({ icon, link }: { icon: React.ReactNode; link: string }) {
         <a href={link}>
             <button className="
       w-10 h-10 flex items-center justify-center 
-      rounded-xl bg-white/5 border border-none mt-5 mb-5
+      rounded-xl bg-white/5 border border-none 
       text-white hover:bg-white/10 hover:border-white/20
       transition-all
-      max-md:w-8 max-md:h-8 max-md:mt-3 hover:cursor-pointer
+      max-md:w-8 max-md:h-8  hover:cursor-pointer
     ">
                 {icon}
             </button>
